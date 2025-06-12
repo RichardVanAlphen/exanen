b9ed412b54c6201097a30c977f7954573ecb7350
RichardVanAlphen <111999466+RichardVanAlphen@users.noreply.github.com>
2025-06-12 17:15:48 +0200

    Update script.js
    inhoud toevoegen

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
-    $('#btnBlog').on('click', function(){
-        $.ajax({
-        url: '../html/blogArtiekelen.html',
-        success: function(data){
-            alert('Heeft de pagina wel kunnen laden.');
-        },
-        error: function(){
-            alert('Heeft de pagina niet kunnen laden.');
-        }
-        })
-    })
+    $.ajax({
+    url: '../html/blogArtiekelen.html',
+    success: function(data){
+        alert('Heeft de pagina wel kunnen laden.');
+        $('#inhoud').append(data)
+    },
+    error: function(){
+        alert('Heeft de pagina niet kunnen laden.');
+    }
+    });
 });
-
